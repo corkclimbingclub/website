880570c932ee227755da0a175aa17bba3d9f801a
Tom Pradat <tom.pradat@gmail.com>
2024-10-01 20:07:20 +0100

    Add 'Become a member' page

--- src/components/JoinButton.jsx
+++ src/components/JoinButton.jsx
@@ -4,8 +4,7 @@ export default function JoinButton(props) {
   return (
     <Button
       tagName="a"
-      target="_blank"
-      href="https://docs.google.com/forms/d/1Q6G2WtbGd-Yqa0_qZMgsY31BFH2PBpaQlrO1FnlQmeE/edit?chromeless=1"
+      href="./become-member"
       {...props}
     />
   );
